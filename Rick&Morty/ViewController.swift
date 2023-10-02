0a58382be7d6caddd83b2e1997d5aeadef41625b
pw-sgussoni <sgussoni@xt.megamedia.es>
2023-10-02 08:46:35 +0200

    Initial Commit

---
+++ Rick&Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rick&Morty
+//
+//  Created by pw-sgussoni on 2/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
